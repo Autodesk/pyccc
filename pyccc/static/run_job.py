8af0e9eb0dec51b88c37934ca5a3c9ae077e38e3
Aaron Virshup <aaronvirshup@Gibarian.local>
2016-05-13 20:59:22 -0700

    Add license and copyright notices

--- pyccc/static/run_job.py
+++ pyccc/static/run_job.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
-# This is a script that drives the remote computation of a function or instance method.
-# It's designed to be launched by the JobLauncher class in cloud.ipy
-# The job.pkl and source.py files will be staged in.
-# result.pkl (and obj_final.pkl for instance methods) file will be staged out
+# Copyright 2016 Autodesk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This is a script that drives the remote computation of a function or instance method.
+It's designed to use the pickled objects produced by the PythonJob class
+"""
 import cPickle as cp
 import traceback as tb
 
