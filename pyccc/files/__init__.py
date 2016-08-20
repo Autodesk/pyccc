843fa87934cd459fa06b99d3a4c48002616a0613
Aaron Virshup <aaronvirshup@Gibarian.local>
2016-08-19 19:48:51 -0700

    Rework and refactor file reference classes to support python 3-style bytes and str classes

---
+++ pyccc/files/__init__.py
@@ -0,0 +1,5 @@
+from .base import *
+from .bytecontainer import *
+from .stringcontainer import *
+from .localfiles import *
+from .remotefiles import *
---
+++ pyccc/tests/test_file_refs.py
@@ -0,0 +1,64 @@
+# -*- coding: UTF-8 -*-
+import pytest
+import uuid
+
+import pyccc
+
+registered_types = {}
+
+
+def typedfixture(*types, **kwargs):
+    """This is a decorator that lets us associate fixtures with one or more arbitrary types.
+    This makes it easy, later, to run the same test on all fixtures of a given type"""
+
+    def fixture_wrapper(func):
+        for t in types:
+            registered_types.setdefault(t, []).append(func.__name__)
+        return pytest.fixture(**kwargs)(func)
+
+    return fixture_wrapper
+
+BYTES_CONTENT = b'abcd\n1234'
+STRING_CONTENT = 'abcd\n1234'
+LINES_CONTENT = ['abcd\n',
+                 '1234']
+
+
+def _gen_tempfile_path():
+    return '/tmp/pyccc_test_%s.txt' % uuid.uuid4()
+
+
+@typedfixture('container')
+def bytescontainer():
+    return pyccc.BytesContainer(BYTES_CONTENT)
+
+
+@typedfixture('container')
+def stringcontainer():
+    return pyccc.StringContainer(STRING_CONTENT)
+
+
+@typedfixture('container')
+def localfile_from_bytes(bytescontainer):
+    return bytescontainer.put(_gen_tempfile_path())
+
+
+@typedfixture('container')
+def localfile_from_string(stringcontainer):
+    return stringcontainer.put(_gen_tempfile_path())
+
+
+@typedfixture('container')
+def localfile_in_memory(bytescontainer):
+    localfile = bytescontainer.put(_gen_tempfile_path())
+    return pyccc.FileContainer(localfile.localpath)
+
+
+@pytest.mark.parametrize('objkey', registered_types['container'])
+def test_file_container(objkey, request):
+    my_container = request.getfuncargvalue(objkey)
+    assert list(my_container) == LINES_CONTENT
+    assert my_container.read() == STRING_CONTENT
+    assert my_container.read('rb') == BYTES_CONTENT
+
+
