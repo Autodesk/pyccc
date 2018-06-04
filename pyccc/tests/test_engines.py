ada98f815adf252e1f8f74a1c0448fa584abe27f
Aaron Virshup <avirshup@gmail.com>
2018-06-04 13:56:47 -0700

    Split out general engine tests for dependency injection tests

--- pyccc/tests/test_engines.py
+++ pyccc/tests/test_engines.py
@@ -6,7 +6,28 @@ import pyccc
 from .engine_fixtures import *
 from . import function_tests
 
-"""Basic test battery for regular and python jobs on all underlying engines"""
+"""
+Basic test battery for regular and python jobs on all underlying engines
+
+This can be used to test external engines (in a hacky way right now):
+
+```python
+import pytest
+from pyccc.tests import engine_fixtures
+
+@pytest.fixture
+def my_engine_fixture():
+    return MyCustomEngine()
+
+engine_fixtures['engine'] = my_engine_fixture
+
+from pyccc.tests.test_engines import *
+```
+
+A less hacky way to do this would be via pytest-testscenarios or similar test generation strategy
+see https://docs.pytest.org/en/latest/example/parametrize.html#a-quick-port-of-testscenarios
+
+"""
 
 PYVERSION = '%s.%s' % (sys.version_info.major, sys.version_info.minor)
 PYIMAGE = 'python:%s-slim' % PYVERSION
@@ -176,21 +197,6 @@ def test_bash_exitcode(fixture, request):
     assert job.exitcode == 35
 
 
-@pytest.fixture
-def set_env_var():
-    import os
-    assert 'NULL123' not in os.environ, "Bleeding environment"
-    os.environ['NULL123'] = 'nullabc'
-    yield
-    del os.environ['NULL123']
-
-
-def test_subprocess_environment_preserved(subprocess_engine, set_env_var):
-    job = subprocess_engine.launch(command='echo $NULL123', image='python:2.7-slim')
-    job.wait()
-    assert job.stdout.strip() == 'nullabc'
-
-
 @pytest.mark.parametrize('fixture', fixture_types['engine'])
 def test_python_exitcode(fixture, request):
     engine = request.getfuncargvalue(fixture)
@@ -348,86 +354,6 @@ def _runcall(fixture, request, function, *args, **kwargs):
     return job.result
 
 
-@pytest.mark.skipif('CI_PROJECT_ID' in os.environ,
-                    reason="Can't mount docker socket in codeship")
-def test_docker_socket_mount_with_engine_option(local_docker_engine):
-    engine = local_docker_engine
-
-    job = engine.launch(image='docker',
-                        command='docker ps -q --no-trunc',
-                        engine_options={'mount_docker_socket': True})
-    job.wait()
-    running = job.stdout.strip().splitlines()
-    assert job.jobid in running
-
-
-@pytest.mark.skipif('CI_PROJECT_ID' in os.environ,
-                    reason="Can't mount docker socket in codeship")
-def test_docker_socket_mount_withdocker_option(local_docker_engine):
-    engine = local_docker_engine
-
-    job = engine.launch(image='docker',
-                        command='docker ps -q --no-trunc',
-                        withdocker=True)
-    job.wait()
-    running = job.stdout.strip().splitlines()
-    assert job.jobid in running
-
-
-def test_docker_volume_mount(local_docker_engine):
-    """
-    Note:
-        The test context is not necessarily the same as the bind mount context!
-        These tests will run in containers themselves, so we can't assume
-        that any directories accessible to the tests are bind-mountable.
-
-        Therefore we just test a named volume here.
-    """
-    import subprocess, uuid
-    engine = local_docker_engine
-    key = uuid.uuid4()
-    volname = 'my-mounted-volume-%s' % key
-
-    # Create a named volume with a file named "keyfile" containing a random uuid4
-    subprocess.check_call(('docker volume rm {vn}; docker volume create {vn}; '
-                          'docker run -v {vn}:/mounted alpine sh -c "echo {k} > /mounted/keyfile"')
-                          .format(vn=volname, k=key),
-                          shell=True)
-
-    job = engine.launch(image='docker',
-                        command='cat /mounted/keyfile',
-                        engine_options={'volumes': {volname: '/mounted'}})
-    job.wait()
-    result = job.stdout.strip()
-    assert result == str(key)
-
-
-def test_readonly_docker_volume_mount(local_docker_engine):
-    engine = local_docker_engine
-    mountdir = '/tmp'
-    job = engine.launch(image='docker',
-                        command='echo blah > /mounted/blah',
-                        engine_options={'volumes':
-                                            {mountdir: ('/mounted', 'ro')}})
-    job.wait()
-    assert isinstance(job.exitcode, int)
-    assert job.exitcode != 0
-
-
-def test_set_workingdir_docker(local_docker_engine):
-    engine = local_docker_engine
-    job = engine.launch(image='docker', command='pwd', workingdir='/testtest-dir-test')
-    job.wait()
-    assert job.stdout.strip() == '/testtest-dir-test'
-
-
-def test_set_workingdir_subprocess(subprocess_engine, tmpdir):
-    engine = subprocess_engine
-    job = engine.launch(image=None, command='pwd', workingdir=str(tmpdir))
-    job.wait()
-    assert job.stdout.strip() == str(tmpdir)
-
-
 @pytest.mark.parametrize('fixture', fixture_types['engine'])
 def test_clean_working_dir(fixture, request):
     """ Because of some weird results that seemed to indicate the wrong run dir
@@ -438,7 +364,7 @@ def test_clean_working_dir(fixture, request):
     assert job.stdout.strip() == ''
 
 
-class no_context():  # context manager that does nothing that can be used conditionaly
+class no_context():  # context manager that does nothing -- can be used conditionally
     def __enter__(self):
         return None
     def __exit__(self, exc_type, exc_value, traceback):
